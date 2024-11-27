bb5850290ef20e1fd3c292029459133b85c70389
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-11-27 21:46:42 +0000

    add draw player

--- dungeon_crawler/main.py
+++ dungeon_crawler/main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from character import Character
 
 pygame.init()
 
@@ -9,14 +10,22 @@ pygame.init()
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon Crawler")
 
+# create player
+player = Character(100, 100)
+
 # main game loop
 run = True
 while run:
+    
+    # draw player on screen
+    player.draw(screen)
 
     # event handler
     for event in pygame.event.get():
         if event == pygame.quit:
             run = False
+    
+    pygame.display.update()
 
 
 pygame.quit()
